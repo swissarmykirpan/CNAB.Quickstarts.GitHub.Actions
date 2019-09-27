3eb81be5dcd4a8b87bf4b18e6b483a2e06ef310d
Mike Larah <Mike.Larah@endjin.com>
2019-09-27 10:43:57 +0100

    Added action for installing Porter

---
+++ install-porter/entrypoint.sh
@@ -0,0 +1,29 @@
+install_path=$1
+porter_url=$2
+porter_version=$3
+feed_url=$4
+mixins=$5
+mixins_version=$6
+
+echo "Installing porter to ${install_path}"
+
+mkdir -p ${install_path}
+curl -fLo ${install_path} "${porter_url}/${porter_version}/porter-linux-amd64"
+chmod +x ${install_path}
+cp "${install_path}/porter" "${install_path}/porter-runtime"
+
+echo Installed "$("${install_path}/porter" version)"
+
+
+echo "Installing mixins"
+
+IFS=',' read -ra mixins_array <<< "$mixins"
+
+for mixin in "${mixins_array[@]}"
+do
+    "${install_path}/porter" mixin install $mixin --version "${mixins_version}" --feed-url "${feed_url}"
+done
+
+echo "Installed mixins"
+
+echo ::set-output name=install_path::${install_path}
