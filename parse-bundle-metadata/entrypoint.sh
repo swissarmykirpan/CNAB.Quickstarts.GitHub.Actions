257f167690cbf17f2129ef556f848d68c428574a
Mike Larah <Mike.Larah@endjin.com>
2019-09-27 16:02:34 +0100

    Use path relative to home

--- parse-bundle-metadata/entrypoint.sh
+++ parse-bundle-metadata/entrypoint.sh
@@ -2,19 +2,18 @@
 
 metadata_path=$1
 
-cd $GITHUB_WORKSPACE
-ls
+full_path="$GITHUB_WORKSPACE/$metadata_path"
 
-echo "Metadata path: ${metadata_path}"
+echo "Full metadata path: $full_path"
 
 echo "Installing yq"
 python -m pip install --upgrade pip yq
 echo "Installed yq"
 
-name=$(yq .name ${metadata_path} -r)
+name=$(yq .name ${full_path} -r)
 echo "name: ${name}"
 
-invocation_image=$(yq .invocationImage ${metadata_path} -r)
+invocation_image=$(yq .invocationImage ${full_path} -r)
 echo "invocation_image: ${invocation_image}"
 
 echo ::set-output name=name::${name}
