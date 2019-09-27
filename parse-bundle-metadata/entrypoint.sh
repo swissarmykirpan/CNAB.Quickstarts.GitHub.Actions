e7d9b271e77a501c5fe1e37aa4c0ba1e6e9f35b9
Mike Larah <Mike.Larah@endjin.com>
2019-09-27 15:39:36 +0100

    Added action for parsing metadata

---
+++ parse-bundle-metadata/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+metadata_path=$1
+
+echo "${metadata_path}"
+
+echo "Installing yq"
+python -m pip install --upgrade pip yq
+echo "Installed yq"
+
+name=$(yq .name ${metadata_path} -r)
+echo "name: ${name}"
+
+invocation_image=$(yq .invocationImage ${metadata_path} -r)
+echo "invocation_image: ${invocation_image}"
+
+echo ::set-output name=name::${name}
+echo ::set-output name=invocation_image::${invocation_image}
