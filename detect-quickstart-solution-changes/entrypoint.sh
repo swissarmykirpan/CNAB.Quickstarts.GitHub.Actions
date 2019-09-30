2b221a30e0c73e8114a4b5a607d3dc894a77cab2
Mike Larah <Mike.Larah@endjin.com>
2019-09-30 12:49:00 +0100

    Add unsupported path

--- detect-quickstart-solution-changes/entrypoint.sh
+++ detect-quickstart-solution-changes/entrypoint.sh
@@ -5,16 +5,21 @@ trigger=$2
 pr_number=$3
 source_version=$4
 
-if [ "${trigger}" == "push" ]; then
+if [ "${trigger}" == "push" ]
+then
     commit_uri=https://api.github.com/repos/${repo_name}/commits/${source_version}
     echo "Merge Commit uri: ${commit_uri}"
     files=$(curl "${commit_uri}"|jq '[.files[].filename]') 
-fi
 
-if [ "${trigger}" == "pull_request" ]; then
+elif [ "${trigger}" == "pull_request" ]
+then
     pr_uri="https://api.github.com/repos/${repo_name}/pulls/${pr_number}/files"
     echo "PR uri: ${pr_uri}"
     files=$(curl "${pr_uri}"|jq '[.[].filename]') 
+
+else
+    echo "Unsupported trigger type: $trigger"
+    exit 1
 fi
 
 echo "Files changed:"
