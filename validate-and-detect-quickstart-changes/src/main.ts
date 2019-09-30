0af60f2a5b5435042b0fed20f4b018dc84e1b68b
Mike Larah <Mike.Larah@endjin.com>
2019-09-30 17:55:35 +0100

    Call getFiles

--- validate-and-detect-quickstart-changes/src/main.ts
+++ validate-and-detect-quickstart-changes/src/main.ts
@@ -1,7 +1,20 @@
 import * as core from '@actions/core';
+import './functions';
+import { getFiles } from './functions';
 
 async function run() {
   try {
+    let trigger = core.getInput('trigger');
+    let repoName = core.getInput('repo_name');
+    let sourceVersion = core.getInput('source_version');
+    let prNumberStr = core.getInput('pr_number');
+
+    let prNumber : number | undefined;
+    if (prNumberStr) {
+        prNumber = parseInt(prNumberStr);
+    }
+
+    getFiles(trigger, repoName, sourceVersion, prNumber);
 
   } catch (error) {
     core.setFailed(error.message);
