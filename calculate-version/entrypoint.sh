93e902d8c81d48f7b5658adeec008bf79e866e4c
Mike Larah <Mike.Larah@endjin.com>
2019-10-14 11:18:03 +0100

    Added action for calculating version

---
+++ calculate-version/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+manifest_path=$1
+
+full_manifest_path="$GITHUB_WORKSPACE/$manifest_path"
+quickstart_dir=$(realpath $full_manifest_path/..)
+
+echo "Installing yq"
+python -m pip install --upgrade pip yq
+echo "Installed yq"
+
+version=$(yq .version ${full_manifest_path} -r)
+echo "version: ${version}"
+
+gitversion_config_path=$quickstart_dir/gitversion.yml
+contents="next-version: $version"
+
+echo "Writing temporary Gitversion config file with contents '$contents' to path '${gitversion_config_path}'"
+echo $contents > $gitversion
+
+cd $quickstart_dir
+
+dotnet /app/GitVersion.dll -nocache -output buildserver -exec /bin/bash -execargs "-c \"echo $GitVersion_FullSemVer > $GITHUB_WORKSPACE/version.txt\""
+
+GitVersion_FullSemVer=$(cat $GITHUB_WORKSPACE/version.txt)
+
+echo ::set-output name=fullsemver::$GitVersion_FullSemVer
